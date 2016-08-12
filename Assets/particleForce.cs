8903f779ad40c8375b4e507f46bbf2ae91853298
Si Li <omikun@gmail.com>
2016-08-11 23:09:29 -0700

    Butchered Lennard-Jones potential
    Used it to control velocity instead of... potential?

--- Assets/particleForce.cs
+++ Assets/particleForce.cs
@@ -1,50 +1,89 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class particleForce : MonoBehaviour {
-
+    public float epsilon = 1.0f;
+    public float delta = 0.3f;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate () {
+    Vector3 calculateForce(Vector3 a, Vector3 b, float threshold, float accel)
+    {
+        var diff = a - b;
+        float magSqr = diff.sqrMagnitude;
+        float mag = diff.magnitude;
+        float vel = 0.0f;
+        if (magSqr < threshold)
+        {
+            if (false)
+                vel = accel / magSqr;
+            else { 
+                vel = -1 * 4 * epsilon * (float)(Math.Pow(delta / mag, 4) - Math.Pow(delta / mag, 2));
+                //vel = (Math.Abs(vel) > 0.5f) ? 0.5f*Math.Sign(vel) : vel;
+                if (vel < -0.1f)
+                    vel = -0.1f;
+            }
+        }
+        return vel * diff;
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
         GameObject[] hAtoms = GameObject.FindGameObjectsWithTag("h");
         GameObject[] cAtoms = GameObject.FindGameObjectsWithTag("c");
         Debug.Log("number of atoms attraction: " + hAtoms.Length);
-        foreach(GameObject h1 in hAtoms)
+
+        //carbon-carbon pairs
+        foreach (GameObject c1 in cAtoms)
         {
-            foreach(GameObject c1 in cAtoms)
+            foreach (GameObject c2 in cAtoms)
             {
-                Vector3 diff = c1.transform.position - h1.transform.position;
-                float magSqr = diff.sqrMagnitude;
-                if (magSqr < .45f)
+                if (c1 != c2)
                 {
-                    float vel = 0.05f / magSqr / magSqr;
-                    //Debug.Log("distance: " + magSqr + " vel: " + vel);
-                    vel = (vel > 1) ? 1 : vel;
-                    h1.GetComponent<Rigidbody>().velocity += vel * diff;
-                    c1.GetComponent<Rigidbody>().velocity -= vel * diff;
+                    var vel = calculateForce(c1.transform.position, c2.transform.position, 0.45f, 0.05f);
+                    c2.GetComponent<Rigidbody>().velocity += vel;
+                    c1.GetComponent<Rigidbody>().velocity -= vel;
                 }
             }
+        }
+        foreach (GameObject h1 in hAtoms)
+        {
+            //hydrogen-carbon pairs
+            foreach(GameObject c1 in cAtoms)
+            {
+                var vel = calculateForce(c1.transform.position, h1.transform.position, 0.45f, 0.05f);
+                h1.GetComponent<Rigidbody>().velocity += vel;
+                c1.GetComponent<Rigidbody>().velocity -= vel;
+            }
+            //hydrogen-hydrogen pairs
             foreach (GameObject h2 in hAtoms)
             {
                 if (h1 != h2)
                 {
-                    Vector3 diff = h1.transform.position - h2.transform.position;
-                    float magSqr = diff.sqrMagnitude;
-                    if (magSqr < .25f)
-                    {
-                        float vel = 0.01f / magSqr / magSqr;
-                        //Debug.Log("distance: " + magSqr + " vel: " + vel);
-                        vel = (vel > 1) ? 1 : vel;
-                        h2.GetComponent<Rigidbody>().velocity -= vel * diff;
-                    }
+                    var vel = calculateForce(h2.transform.position, h1.transform.position, 0.25f, 0.01f);
+                    h2.GetComponent<Rigidbody>().velocity += vel;
                 }
 
             }
-
         }
+
+        //apply max speed
+#if true
+        foreach(GameObject h1 in hAtoms)
+        {
+            var mag = h1.GetComponent<Rigidbody>().velocity.magnitude;
+            if (mag > 2)
+                h1.GetComponent<Rigidbody>().velocity /= mag/2;
+        }
+        foreach (GameObject c1 in cAtoms)
+        {
+            var mag = c1.GetComponent<Rigidbody>().velocity.magnitude;
+            if (mag > 2)
+                c1.GetComponent<Rigidbody>().velocity /= mag/2;
+        }
+#endif
+
 	}
 }
